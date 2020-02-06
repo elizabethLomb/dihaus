bb22f525f5b1a78a2aed895265685a4c772adc0f
Elizabeth Lombardi <elizabeth.lombardi7@gmail.com>
2020-02-06 20:11:07 +0100

    App configuration and confi folder

---
+++ config/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+//const authMiddleware = require('../middlewares/auth.middleware')
+const upload = require('./cloudinary.config');
+
+/** Controllers ---- */
+
+module.exports = router;
