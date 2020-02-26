9cc19d6fd315c7f7ce5d3b8c5bf37a5e72671dd7
Elizabeth Lombardi <elizabeth.lombardi7@gmail.com>
2020-02-26 20:02:39 +0100

    Added mote comforts con random and changed rules for each

--- constants/rules.js
+++ constants/rules.js
@@ -1,7 +1,10 @@
 const rules = [
   'Se permite fumar',
   'Se permite mascotas',
-  'Se permiten niños'
+  'Se permiten niños',
+  'No se permite fumar',
+  'No se permiten mascotas',
+  'No se permite fumar'
 ];
 
 module.exports = rules;
